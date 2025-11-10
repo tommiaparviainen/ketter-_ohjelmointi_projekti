e0ef5100e6c2776df74219237d11b4000214cbe4
kaisa260 <kaisa.kyyra@student.lab.fi>
2025-11-10 14:11:29 +0200

    sqlteidosto

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using MySql.Data;
+
+namespace sqlperunat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string mysqlconn = "server=127.0.0.1;user=root;database=perunat;uid=root;password;";
+            MySqlConnection connection = new MySqlConnection(mysqlconn);
+
+            try
+            {
+                connection.Open();
+                Console.WriteLine("Connection successful!");
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+
+            finally
+            {
+                connection.Close();
+            }
+        }
+    }
+}
